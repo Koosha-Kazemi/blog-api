dce94e8b8b360ae82dbb5ee9b9749aadd59ed5c9
Koosha-bit <kooshakazemig@gmail.com>
2024-08-24 12:48:18 +0330

    add field of custom user to UserAdmin

--- Blog/Account/admin.py
+++ Blog/Account/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+UserAdmin.fieldsets[2][1]['fields'] = (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    'is_writer',
+                    "groups",
+                    "user_permissions",
+
+         )
+
+
+admin.site.register(User, UserAdmin)
