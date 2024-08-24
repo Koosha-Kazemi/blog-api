b7d51c91af856a2563819fc72dbaa0a6b70e9259
Koosha-bit <kooshakazemig@gmail.com>
2024-08-24 19:16:14 +0330

    create  Genres model

--- Blog/BlogApi/models.py
+++ Blog/BlogApi/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length = 12)
+
+    def __str__(self):
+        return self.name
+
