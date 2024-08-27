85acea9d73b469ed437ff95ad79fccb7e1208399
Koosha-bit <kooshakazemig@gmail.com>
2024-08-27 16:19:08 +0330

    create View for list of post

--- Blog/BlogApi/views.py
+++ Blog/BlogApi/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Posts
+from .serializer import PostSerializer
+
+
+class PostList(generics.ListAPIView):
+    queryset = Posts.objects.all()
+    serializer_class = PostSerializer
