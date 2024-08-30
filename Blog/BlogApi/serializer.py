15d1f0e1e8ac3b9ce8a343bf29cb52bfd789f639
Koosha <kooshakazemig@gmail.com>
2024-08-30 11:09:20 +0330

    update CommentSerializer

--- Blog/BlogApi/serializer.py
+++ Blog/BlogApi/serializer.py
@@ -8,13 +8,10 @@ from .models import Genres, Posts, Comments
 User = get_user_model()
 
 
-
-
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genres
         fields = ('name',)
-
 
 
 
@@ -25,27 +22,23 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 
-
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SerializerMethodField(read_only = True)
-   
-
     class Meta:
             model = Posts
             fields = '__all__'
 
-
-    def get_author(self,obj):
+    @staticmethod
+    def get_author(obj):
          return obj.author.username
-    
+
+
     def to_representation(self, instance):
          representation = super().to_representation(instance)
          representation['genre'] = [genre.name for genre in instance.genre.all()]
          return representation
-         
 
-    
-    
+
     def create(self, validated_data):
          user = self.context['request'].user
          validated_data['author'] = user
@@ -55,7 +48,6 @@ class PostSerializer(serializers.ModelSerializer):
          return post
     
 
-   
     def update(self,instance,validata_data):
          instance.title = validata_data.get('title',instance.title)
          instance.text = validata_data.get('text',instance.text)
@@ -63,11 +55,26 @@ class PostSerializer(serializers.ModelSerializer):
          instance.genre.set(genre_data)
          instance.save()
          return instance
-    
+
 
 
 
 class CommentSerializer(serializers.ModelSerializer):
-     class Meta:
-          model = Comments
-          fields = '__all__'
+    class Meta:
+        model = Comments
+        fields = '__all__'
+
+    def create(self, validated_data):
+        request = self.context['request']
+        post = request.data.get('post')
+        user = request.user
+        validated_data['post'] = post
+        validated_data['user'] = user
+        return Comments.objects.create(**validated_data)
+
+    def update(self, instance, validate_data):
+        instance.comment = validate_data.get('comment', instance.comment)
+        instance.reply = validate_data.get('reply', instance.reply)
+        instance.comment = validate_data.get('like_dislike', instance.like_dislike)
+        instance.save()
+        return instance
