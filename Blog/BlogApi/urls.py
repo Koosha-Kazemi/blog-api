b8e654cb8424cd259c7f702d7fb34abf4d3c862e
Koosha <kooshakazemig@gmail.com>
2024-09-06 16:40:01 +0330

    create url for PostDetail

--- Blog/BlogApi/urls.py
+++ Blog/BlogApi/urls.py
@@ -4,6 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name = 'post_list'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail')
+
     
 ]
 
