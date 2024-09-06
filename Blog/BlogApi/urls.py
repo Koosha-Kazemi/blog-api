8d2097f1993db43e9061f5d4abf437e62bd78ae0
Koosha <kooshakazemig@gmail.com>
2024-09-06 20:45:16 +0330

    create url for posting comment

--- Blog/BlogApi/urls.py
+++ Blog/BlogApi/urls.py
@@ -4,7 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name = 'post_list'),
-    path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail')
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('posts/<int:pk>/comment/', CreateComment.as_view(), name='comment_create')
 
     
 ]
