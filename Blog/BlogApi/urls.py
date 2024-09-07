19ddc0c236b81ef1638f254e5de9dd9b7cadbe04
Koosha <kooshakazemig@gmail.com>
2024-09-07 18:18:28 +0330

    create url for liking comment

--- Blog/BlogApi/urls.py
+++ Blog/BlogApi/urls.py
@@ -5,7 +5,8 @@ from .views import *
 urlpatterns = [
     path('posts/', PostView.as_view(), name = 'post_list'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
-    path('posts/<int:pk>/comment/', CreateComment.as_view(), name='comment_create')
+    path('posts/<int:pk>/comment/', CreateComment.as_view(), name='comment_create'),
+    path('posts/<int:pk>comment/<int:pk>/like', CreateLike.as_view(), name = 'comment_like'),
 
     
 ]
