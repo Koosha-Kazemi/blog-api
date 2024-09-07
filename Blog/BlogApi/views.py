0d322c07b34de43f1fddf3c4a8303752e9708829
Koosha <kooshakazemig@gmail.com>
2024-09-07 18:15:57 +0330

    create CreateLike view

--- Blog/BlogApi/views.py
+++ Blog/BlogApi/views.py
@@ -4,8 +4,8 @@ from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
-from .models import Posts, Comments
-from .serializer import PostSerializer, PostDetailSerializer, CreateCommentSerializer
+from .models import Posts, Comments, Likes
+from .serializer import PostSerializer, PostDetailSerializer, CreateCommentSerializer, LikeSerializer
 
 
 class PostView(generics.ListAPIView):
@@ -26,5 +26,14 @@ class CreateComment(generics.CreateAPIView):
     def perform_create(self, serializer):
         post = get_object_or_404(Posts, id = self.kwargs['pk'])
         serializer.save(post = post)
+
+
+class CreateLike(generics.CreateAPIView):
+    queryset = Likes.objects.all()
+    serializer_class = LikeSerializer
+
+    def perform_create(self, serializer):
+        comment = get_object_or_404(Comments, id =self.kwargs['comment_id'])
+        serializer.save(comment=comment)
 
 
