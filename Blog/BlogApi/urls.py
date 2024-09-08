df5c0b608413de176e9ea3d9f75cbd47695056d0
Koosha <kooshakazemig@gmail.com>
2024-09-08 17:08:27 +0330

    edit url file

--- Blog/BlogApi/urls.py
+++ Blog/BlogApi/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path('posts/', PostView.as_view(), name = 'post_list'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('posts/<int:pk>/comment/', CreateComment.as_view(), name='comment_create'),
-    path('posts/<int:pk>comment/<int:pk>/like', CreateLike.as_view(), name = 'comment_like'),
+    path('posts/<int:pk>/comment/<int:comment_id>/like/', CreateLike.as_view(), name='comment_like'),
 
-    
 ]
 
