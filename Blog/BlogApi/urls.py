e40e9c5b655b904c959ba603fde38437a34ccc50
Koosha <kooshakazemig@gmail.com>
2024-09-17 19:50:08 +0330

    create url for create reply

--- Blog/BlogApi/urls.py
+++ Blog/BlogApi/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('posts/<int:pk>/comment/', CreateComment.as_view(), name='comment_create'),
     path('posts/<int:pk>/comment/<int:comment_id>/like/', CreateLike.as_view(), name='comment_like'),
     path('posts/<int:pk>/comment/<int:comment_id>/reset-like/', ResetLike.as_view(), name='reset_like'),
-    path('comment/<int:comment_id>/reply/',CreateReply.as_view(), name='reply'),
+    path('comment/<int:comment_id>/reply/',CreateReply.as_view(), name='reply_create'),
 
 ]
 
