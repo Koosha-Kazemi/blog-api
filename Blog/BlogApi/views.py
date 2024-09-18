7d24d576366c77438b7d8ebb4e9614c8ffddae3a
Koosha <kooshakazemig@gmail.com>
2024-09-18 08:05:45 +0330

    update View file

--- Blog/BlogApi/views.py
+++ Blog/BlogApi/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 
 from .models import Posts, Comments, Likes
 from .serializer import PostSerializer, PostDetailSerializer, CreateCommentSerializer, LikeSerializer, \
-    CommentReplySerializer
+CommentSerializer
 
 
 class PostView(generics.ListAPIView):
