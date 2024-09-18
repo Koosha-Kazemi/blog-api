f1fe11db16bd10c583e4f4b52c691837d7b69201
Koosha <kooshakazemig@gmail.com>
2024-09-18 07:21:41 +0330

    add JWT

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
+
     'drf_yasg',
     'Account',
     'BlogApi',
@@ -135,6 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+           'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
