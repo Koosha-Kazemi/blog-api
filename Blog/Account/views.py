e30a64450d5de65396a0adbc8edeff6417de9305
Koosha <kooshakazemig@gmail.com>
2024-09-19 13:26:14 +0330

    create RegisterUser view

--- Blog/Account/views.py
+++ Blog/Account/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from Account.serializer import RegisterUserSerializer
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+
+User = get_user_model()
+
+class RegisterUser(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterUserSerializer
+
+
+    def perform_create(self, serializer):
+        password = make_password(self.request.data.get('password'))
+        serializer.save(
+            password = password
+        )
+
+
+
+
