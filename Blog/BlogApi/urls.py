85499c5f06de7109bdc7ee17cb9305e1ec61a874
Koosha <kooshakazemig@gmail.com>
2024-09-21 23:48:08 +0330

    create url for list of comments

--- Blog/BlogApi/urls.py
+++ Blog/BlogApi/urls.py
@@ -1,3 +1,5 @@
+from token import CIRCUMFLEX
+
 from django.urls import path
 
 from .views import *
@@ -6,6 +8,7 @@ urlpatterns = [
     path('posts/', PostListCerate.as_view(), name = 'post_ListCreate'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('posts/<int:pk>/comment/', CreateComment.as_view(), name='comment_create'),
+    path('posts/<int:pk>/comment/list', CommentList.as_view(), name='comment_list'),
     path('comment/<int:comment_id>/like/', CreateLike.as_view(), name='comment_like'),
     path('comment/<int:comment_id>/reset-like/', ResetLike.as_view(), name='reset_like'),
     path('comment/<int:comment_id>/reply/', ReplyComment.as_view(), name='reply_create'),
