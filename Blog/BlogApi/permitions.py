cd58e637c072c31a44241f450040b6550b7af146
Koosha <kooshakazemig@gmail.com>
2024-09-21 09:37:25 +0330

    update permition file

--- Blog/BlogApi/permitions.py
+++ Blog/BlogApi/permitions.py
@@ -1,6 +1,6 @@
 from rest_framework.permissions import BasePermission
 
-class IsAdminOrSuperUser(BasePermission):
+class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return  request.user.is_superuser or request.user.is_staff
 
