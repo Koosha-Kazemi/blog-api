9e2a328fb629340ce97e3a4722b72fe47c3010ce
Koosha <kooshakazemig@gmail.com>
2024-09-21 09:22:55 +0330

    create IsWriterOrAdminOrReadOnly

--- Blog/BlogApi/permitions.py
+++ Blog/BlogApi/permitions.py
@@ -3,3 +3,14 @@ from rest_framework.permissions import BasePermission
 class AdminOrSuperUser(BasePermission):
     def has_permission(self, request, view):
         return  request.user.is_staff or request.user.is_superuser
+
+
+class IsWriterOrAdminOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+                request.user.is_superuser or
+                request.user.is_staff or
+                request.user == obj
+
+        )
+
