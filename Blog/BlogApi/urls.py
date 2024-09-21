0e58c2cf5fcf3a840d6d966a9f63db86b1a0fc88
Koosha <kooshakazemig@gmail.com>
2024-09-21 23:01:03 +0330

    update urls file

--- Blog/BlogApi/urls.py
+++ Blog/BlogApi/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('posts/', PostListCerate.as_view(), name = 'post_ListCreate'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('posts/<int:pk>/comment/', CreateComment.as_view(), name='comment_create'),
-    path('posts/<int:pk>/comment/<int:comment_id>/like/', CreateLike.as_view(), name='comment_like'),
-    path('posts/<int:pk>/comment/<int:comment_id>/reset-like/', ResetLike.as_view(), name='reset_like'),
+    path('comment/<int:comment_id>/like/', CreateLike.as_view(), name='comment_like'),
+    path('comment/<int:comment_id>/reset-like/', ResetLike.as_view(), name='reset_like'),
     path('comment/<int:comment_id>/reply/', ReplyComment.as_view(), name='reply_create'),
 
 ]
