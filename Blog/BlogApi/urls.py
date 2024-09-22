2b101970da4e5ac8dfa2c1276d5bde3f3a70d696
Koosha <kooshakazemig@gmail.com>
2024-09-22 05:27:57 +0330

    create url for accept comment

--- Blog/BlogApi/urls.py
+++ Blog/BlogApi/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('posts/<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('posts/<int:pk>/comment/', CreateComment.as_view(), name='comment_create'),
     path('posts/<int:pk>/comment/list', CommentList.as_view(), name='comment_list'),
+    path('comment/<int:comment_id>/accept/', AcceptComment.as_view(), name='accept_like'),
     path('comment/<int:comment_id>/like/', CreateLike.as_view(), name='comment_like'),
     path('comment/<int:comment_id>/reset-like/', ResetLike.as_view(), name='reset_like'),
     path('comment/<int:comment_id>/reply/', ReplyComment.as_view(), name='reply_create'),
