11d3caa76e863bdd0e76f50828754949876aa23f
Koosha <kooshakazemig@gmail.com>
2024-09-29 06:41:31 +0330

    update register user view

--- Blog/Account/views.py
+++ Blog/Account/views.py
@@ -1,12 +1,26 @@
 from rest_framework import generics
+from rest_framework.response import Response
+
+from django.contrib.auth import get_user_model
+
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from Account.serializer import RegisterUserSerializer
 
-from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
 
 class RegisterUser(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegisterUserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            refresh_token = RefreshToken.for_user(user)
+            access_token = str(refresh_token.access_token)
+            return Response({
+               'refresh_token' : refresh_token,
+                'access_token' : access_token
+            })
